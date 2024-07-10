93623cd7a01fca3b6ee35c123dbd387cdd29311f
alan-soka <jevallejo13@gmail.com>
2024-07-10 13:26:03 +0200

    feat: e001 working

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -12,10 +12,52 @@
 
 // 5- identidicar el contenedor e insertar tantos div como componentes filtrados existan
 
+// funcion obtener json to array
+
+
+
 const labelCats = document.querySelectorAll('label')
 console.log(labelCats)
 
+
+
 let dataList = []
+
+// mock fecth
+const arrayDEPrueba = [{
+    "title": "El espejismo",
+    "publish_date": "2024-02-14",
+    "author": "Camilla Läckberg",
+    "resume": "Una novela que mezcla la atmósfera navideña de Estocolmo con un misterio siniestro.",
+    "tags": ["novela negra", "misterio"],
+    "image": "https://prodimage.images-bn.com/pimages/9786073912471_p0_v1_s600x595.jpg"
+},
+{
+    "title": "Bajo tierra seca",
+    "publish_date": "2024-03-22",
+    "author": "César Pérez Gellida",
+    "resume": "Un thriller ambientado en la España rural.",
+    "tags": ["novela negra", "thriller"],
+    "image": "URL_de_la_imagen"
+},
+{
+    "title": "La inquilina silenciosa",
+    "publish_date": "2024-01-11",
+    "author": "Clémence Michallon",
+    "resume": "Una historia de misterio y suspense que te mantendrá al borde de tu asiento.",
+    "tags": ["novela negra", "suspense"],
+    "image": "URL_de_la_imagen"
+},
+{
+    "title": "La mujer fugitiva",
+    "publish_date": "2024-04-05",
+    "author": "Alicia Giménez Bartlett",
+    "resume": "Un nuevo caso de la inspectora Petra Delicado.",
+    "tags": ["novela negra", "policiaco"],
+    "image": "URL_de_la_imagen"
+}
+]
+// ...............................................
 
 async function getArticles() {
     const response = await fetch('../json/libreria.json')
@@ -27,18 +69,54 @@ async function getArticles() {
     return dataList
 }
 
+// funcion obtener categorias
+
 let categorias = []
 
-function getCats(){
-    for (const item of labelCats){
+function getCats() {
+    for (const item of labelCats) {
         categorias.push(item.innerText)
-  
+
     }
 }
 
-getCats()
+// funcion filtrar 
+// let categoriaSeleccionada = []
+let librosFiltrados = []
+function filter() {
+    // categorias dataList
+
+    // identificar filtro seleccionado
+    const inputChecked = document.querySelectorAll('input:checked')
+    const categoriaSelect = inputChecked[0].nextElementSibling.innerText
+
+    // por cada elemento del json, comprobar si el elemento contiene los tags, igual a la categoria seleccionada
+    console.log('elemento')
+
+    for (const item of arrayDEPrueba) {
+        console.log(item.tags)
+        console.log(item.tags.includes(categoriaSelect))
+        console.log('--------------- ----------------------------')
+
+        if (item.tags.includes(categoriaSelect)) {
+            console.log(item + ' si ')
+            librosFiltrados.push(item)
+        }
+
+
+
+    }
+    console.log(librosFiltrados)
+}
+
+
+
 
 // getArticles()
 
+getCats()
+
+
 console.log(dataList)
 console.log(categorias)
+filter()
